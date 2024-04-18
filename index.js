90ec76dd1f7aa44b5e9f84800a92bc85b94be7ec
kikealcocerdz <enrique.alcocer1203@gmail.com>
2024-04-18 14:18:56 +0200

    doble lista y qr

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/product-list-added', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
   io.emit('product list', productList); // Enviar la lista de productos al cliente recién conectado
-
+  io.emit('product list added', productListAdded); // Enviar la lista de productos al cliente recién conectado
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
@@ -42,7 +42,7 @@ io.on('connection', (socket) => {
 
   socket.on('product added voice', (productName) => {
     productListAdded.push(productName);
-    console.log('Current product list:', productListAdded);
+    console.log('Current product list sonido:', productListAdded);
     io.emit('product list added', productListAdded); // Emite la lista de productos a todos los clientes conectados
   }
   );
@@ -59,14 +59,17 @@ app.get('/useractive.html', (req, res) => {
 });
 
 app.get('/cart.html', (req, res) => {
-  // Emitir el evento cuando el usuario se conecta a /useractive.html
+  // Emitir el evento cuando el usuario se conecta a /cart.html
   io.emit('user connected');
-  // Emitir la lista actual de productos al usuario que se conecta
-  io.emit('product list added', productListAdded);
-  // Responder con el archivo useractive.html
+
+  // Emitir la lista actual de productos añadidos al usuario que se conecta a /cart.html
+  console.log('Current product list added:', productListAdded);
+  io.to('cart').emit('product list added', productListAdded); // Cambio aquí
+
+  // Responder con el archivo cart.html
   res.sendFile(join(__dirname, 'cart.html'));
-}
-);
+});
+
 
 
 // Escuchar conexiones de Socket.IO
