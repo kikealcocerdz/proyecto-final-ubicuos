2c6ced5fe2bba648ae70b981d904200e2ce38052
100346155 <100345155@alumnos.uc3m.es>
2024-04-19 23:41:51 +0200

    Selection and rotation added, delete having issues

--- cart/cart.js
+++ cart/cart.js
@@ -1,14 +1,96 @@
 //####################################################################################################################
 //############################################## Eliminar Producto ###################################################
+var socketDelete = io(); // Conecta al mismo servidor que el archivo server.html
+const cartItems = [
+  "lista-placeholder",
+  "product-lista",
+  "product-image",
+  "product-description",
+  "button",
+];
+let productsAll = document.querySelectorAll(".lista-container");
 
+// Si un elemento de la lista de productos se mantiene seleccionado y el dispositivo es girado 180 grados, eliminar el producto
+document.addEventListener("DOMContentLoaded", function () {
+  console.log(document.querySelectorAll(".lista-placeholder"));
+
+  let selected = null;
+  let originalColor = null;
+
+  productsAll.forEach((product) => {
+    product.addEventListener("touchstart", (event) => {
+      console.log(event.target.className);
+      if (cartItems.includes(event.target.className)) {
+        event.preventDefault();
+        let touchMoved = false;
+        console.log("Evento touchstar: ", event.target);
+        // Get the background color of the event
+        let defaultColor = window.getComputedStyle(
+          event.target
+        ).backgroundColor;
+        const touchTimer = setTimeout(() => {
+          if (!touchMoved) {
+            // If the product is selected, change its color
+            if (selected) {
+              selected.style.backgroundColor = originalColor;
+            }
+            selected = event.target;
+            originalColor = defaultColor;
+            selected.style.backgroundColor = "#da692afa";
+          }
+        }, 100);
+        let initialRotation = {
+          alpha: 0,
+          beta: 90,
+          gamma: 0,
+        };
+        let productsAll = [];
+        // Añadir todos los hijos de listacontainer a productsAll
+        productListContainer.childNodes.forEach((product) => {
+          productsAll.push(product);
+        });
+        console.log("Productos en la lista:", productsAll);
+
+        window.addEventListener("deviceorientation", function (event) {
+          if (initialRotation === undefined) {
+            initialRotation.alpha = event.alpha;
+            initialRotation.beta = event.beta;
+            initialRotation.gamma = event.gamma;
+            console.log("Initial rotation:", initialRotation);
+          } else {
+            const rotationDifferenceAlpha = Math.abs(
+              event.alpha - initialRotation.alpha
+            );
+            if (
+              rotationDifferenceAlpha > 90 &&
+              rotationDifferenceAlpha < 90.5
+            ) {
+              console.log("The device has been rotated 90 degrees.");
+              console.log("Product to be deleted:", selected);
+              socketDelete.on("product delete voice", function (selected) {
+                console.log("Lista de productos:", productListAdded);
+                console.log("Podructo a eliminar:", selected);
+                productListAdded.pop(selected);
+                console.log(
+                  "Lista de productos después de eliminar:",
+                  productListAdded
+                );
+                io.emit("product list added", productListAdded);
+              });
+            }
+          }
+        });
+      }
+    });
+  });
+});
 
 //####################################################################################################################
 //############################################ Fin Eliminar Producto #################################################
 //####################################################################################################################
 
-
 //####################################################################################################################
-//############################################ Shake                  #################################################
+//############################################# Agitar para Ordenar ##################################################
 //####################################################################################################################
 // Función para manejar el evento de shake
 function handleShake(event) {
@@ -19,25 +101,24 @@ function handleShake(event) {
 
     const accelerationMagnitude = Math.sqrt(
       accelerationX * accelerationX +
-      accelerationY * accelerationY +
-      accelerationZ * accelerationZ
+        accelerationY * accelerationY +
+        accelerationZ * accelerationZ
     );
 
     const shakeThreshold = 15;
 
     if (accelerationMagnitude > shakeThreshold) {
       // Ordena la lista
-      
-      
+
       // Vibrar solo en respuesta a la sacudida
       if (navigator.vibrate) {
         navigator.vibrate(200);
-      }6
+      }
+      6;
       ordenarLista();
-
     }
   } else {
-    console.log('No se han proporcionado datos de aceleración.');
+    console.log("No se han proporcionado datos de aceleración.");
   }
 }
 
@@ -45,15 +126,15 @@ function handleShake(event) {
 function ordenarLista() {
   const productContainer = document.getElementById("product-list");
   const products = productContainer.querySelectorAll(".lista-placeholder");
-  
+
   // Convertir la lista de nodos a un array para poder ordenarla
   const productsArray = Array.from(products);
-  
+
   // Ordenar los elementos el nombre del producto
   productsArray.sort((a, b) => {
     const nameA = a.id.toUpperCase();
     const nameB = b.id.toUpperCase();
-    
+
     if (nameA < nameB) {
       return -1;
     }
@@ -62,21 +143,19 @@ function ordenarLista() {
     }
     return 0;
   });
-  
+
   // Limpiar el contenedor de productos
   productContainer.innerHTML = "";
-  
+
   // Agregar los elementos ordenados de vuelta al contenedor
-  productsArray.forEach(product => {
+  productsArray.forEach((product) => {
     productContainer.appendChild(product);
   });
 }
 
-
 // Agregar un listener para el evento 'devicemotion'
-window.addEventListener('devicemotion', handleShake);
-
+window.addEventListener("devicemotion", handleShake);
 
 //####################################################################################################################
-//############################################ Shake                  #################################################
+//############################################ Fin Agitar para Ordenar #################################################
 //####################################################################################################################
--- index.js
+++ index.js
@@ -1,90 +1,96 @@
-const express = require('express');
-const { createServer } = require('http');
-const { join } = require('path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("http");
+const { join } = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 // Servir tanto index.html como useractive.html desde la misma ruta
-app.use(express.static(join(__dirname, '')));
+app.use(express.static(join(__dirname, "")));
 
 // Array para almacenar la lista de productos
 let productList = [];
 let productListAdded = [];
 
 // Endpoint GET para obtener la lista de productos
-app.get('/product-list', (req, res) => {
+app.get("/product-list", (req, res) => {
   res.json(productList);
 });
 
-app.get('/product-list-added', (req, res) => {
+app.get("/product-list-added", (req, res) => {
   res.json(productList);
 });
-
 
 // Emitir la lista actual de productos a cualquier usuario que se conecte
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.emit('product list', productList); // Enviar la lista de productos al cliente recién conectado
-  io.emit('product list added', productListAdded); // Enviar la lista de productos al cliente recién conectado
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  io.emit("product list", productList); // Enviar la lista de productos al cliente recién conectado
+  io.emit("product list added", productListAdded); // Enviar la lista de productos al cliente recién conectado
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 
   // Escuchar cuando se añada un nuevo producto y actualizar la lista
-  socket.on('product added', (productName) => {
+  socket.on("product added", (productName) => {
     productList.push(productName);
-    console.log('Current product list:', productList);
-    io.emit('product list', productList); // Emite la lista de productos a todos los clientes conectados
+    console.log("Current product list:", productList);
+    io.emit("product list", productList); // Emite la lista de productos a todos los clientes conectados
   });
 
-  socket.on('product added voice', (productName) => {
+  socket.on("product added voice", (productName) => {
     productListAdded.push(productName);
-    console.log('Current product list sonido:', productListAdded);
-    io.emit('product list added', productListAdded); // Emite la lista de productos a todos los clientes conectados
-  }
-  );
+    console.log("Current product list sonido:", productListAdded);
+    io.emit("product list added", productListAdded); // Emite la lista de productos a todos los clientes conectados
+  });
+  socket.on("product deleted voice", (productName) => {
+    productListAdded.pop(productName);
+    console.log("Current product list sonido:", productListAdded);
+    io.emit("product list added", productListAdded); // Emite la lista de productos a todos los clientes conectados
+  });
+  socket.on("product deleted", (productName) => {
+    productList.pop(productName);
+    console.log("Current product list:", productList);
+    io.emit("product list", productList); // Emite la lista de productos a todos los clientes conectados
+  });
 });
 
 // Manejar la conexión de usuario a /useractive.html
-app.get('/useractive.html', (req, res) => {
+app.get("/useractive.html", (req, res) => {
   // Emitir el evento cuando el usuario se conecta a /useractive.html
-  io.emit('user connected');
+  io.emit("user connected");
   // Emitir la lista actual de productos al usuario que se conecta
-  io.emit('product list', productList);
+  io.emit("product list", productList);
   // Responder con el archivo useractive.html
-  res.sendFile(join(__dirname, './useractive/useractive.html'));
+  res.sendFile(join(__dirname, "./useractive/useractive.html"));
 });
 
-app.get('/cart.html', (req, res) => {
+app.get("/cart.html", (req, res) => {
   // Emitir el evento cuando el usuario se conecta a /cart.html
-  io.emit('user connected');
+  io.emit("user connected");
 
   // Emitir la lista actual de productos añadidos al usuario que se conecta a /cart.html
-  console.log('Current product list added:', productListAdded);
-  io.to('cart').emit('product list added', productListAdded); // Cambio aquí
+  console.log("Current product list added:", productListAdded);
+  io.to("cart").emit("product list added", productListAdded); // Cambio aquí
 
   // Responder con el archivo cart.html
-  res.sendFile(join(__dirname, './cart/cart.html'));
+  res.sendFile(join(__dirname, "./cart/cart.html"));
 });
 
-app.get('/cajero.html', (req, res) => {
+app.get("/cajero.html", (req, res) => {
   // Emitir el evento cuando el usuario se conecta a /cart.html
-  io.emit('user connected');
+  io.emit("user connected");
 
   // Emitir la lista actual de productos añadidos al usuario que se conecta a /cart.html
-  console.log('Current product list added:', productListAdded);
-  io.to('cajero').emit('product list added', productListAdded); // Cambio aquí
+  console.log("Current product list added:", productListAdded);
+  io.to("cajero").emit("product list added", productListAdded); // Cambio aquí
 
   // Responder con el archivo cart.html
-  res.sendFile(join(__dirname, './QRCODE/cajero.html'));
+  res.sendFile(join(__dirname, "./QRCODE/cajero.html"));
 });
-
-
 
 // Escuchar conexiones de Socket.IO
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+  console.log("Server running at http://localhost:3000");
 });
